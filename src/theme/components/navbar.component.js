168250a85bbe24636d87a3669925332c53fbe1ec
Julián Andrés Chilito <julian.chilitoz@gmail.com>
2021-01-09 23:40:22 -0500

    fix: corrige diseño principal teniendo en cuenta las rutas no privadas

--- src/theme/components/navbar.component.js
+++ src/theme/components/navbar.component.js
@@ -1,21 +1,26 @@
 import { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { todoActions } from '../../redux/actions';
 
-export default class Navbar extends Component {
+class Navbar extends Component {
   constructor(props) {
-    super(props);
+		super(props);
+		this.props.allTodos();
   }
 
   render() {
+		const totalTodos = this.props.todos.filter((todo) => todo.status.id === 'PENDING');
     return (
       <nav className="bg-gray-800 pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
         <div className="flex flex-wrap items-center">
           <div className="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white">
-            <a href="#">
+            <Link to="/dashboard">
               <span className="text-xl pl-2">
                 <i className="em em-grinning"></i>
               </span>
-            </a>
+            </Link>
           </div>
 
           <div className="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2"></div>
@@ -23,13 +28,15 @@ export default class Navbar extends Component {
           <div className="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end">
             <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
               <li className="flex-1 md:flex-none md:mr-3">
-                <a className="inline-block py-2 px-4 text-white no-underline" href="#"><FontAwesomeIcon className="text-xl" icon={['fas', 'search']} /></a>
+                <Link className="inline-block py-2 px-4 text-white no-underline" to="/dashboard"><FontAwesomeIcon className="text-xl" icon={['fas', 'search']} /></Link>
               </li>
               <li className="flex-1 md:flex-none md:mr-3">
-                <a className="relative inline-block py-2 px-4 text-white no-underline hover:text-underline" href="#">
+                <Link className="relative inline-block py-2 px-4 text-white no-underline hover:text-underline" to="/todos">
                   <FontAwesomeIcon className="text-xl" icon={['fas', 'bell']} />
-                  <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-2 bg-red-600 rounded-full">99</span>
-                </a>
+                  <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-2 bg-red-600 rounded-full">
+										{totalTodos.length}
+									</span>
+                </Link>
               </li>
               <li className="flex-1 md:flex-none md:mr-3">
                 <div className="relative inline-block">
@@ -37,17 +44,11 @@ export default class Navbar extends Component {
                     <span className="pr-2">
                       <i className="em em-robot_face"></i>
                     </span>
-                    Hi, User
-                    <svg className="h-3 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    {this.props.auth.user.user.name} 
+                    <svg className="h-3 fill-current inline invisible md:visible" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                       <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                     </svg>
                   </button>
-                  <div id="myDropdown" className="absolute bg-gray-800 text-white right-0 mt-3 p-3 overflow-auto z-30 invisible">
-                    <a href="#" className="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i className="fa fa-user fa-fw"></i> Profile</a>
-                    <a href="#" className="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i className="fa fa-cog fa-fw"></i> Settings</a>
-                    <div className="border border-gray-800"></div>
-                    <a href="#" className="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i className="fas fa-sign-out-alt fa-fw"></i> Log Out</a>
-                  </div>
                 </div>
               </li>
             </ul>
@@ -57,3 +58,19 @@ export default class Navbar extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    alert: state.alert,
+		auth: state.auth,
+		todos: state.todos
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+		allTodos: () => dispatch(todoActions.all()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
--- src/theme/dashboard.theme.js
+++ src/theme/dashboard.theme.js
@@ -6,8 +6,14 @@ export default function DashboardLayout({children}) {
   return (
     <div className="bg-gray-50 font-sans leading-normal tracking-normal">
       <Navbar />
-      <Sidebar />
-      {children}
+      <div className="flex flex-col md:flex-row">
+				<Sidebar />
+				<div className="main-content flex-1 bg-gray-100 mt-12 md:mt-12 md:ml-48 pb-24 md:pb-5">
+					<div className="flex flex-wrap p-5 mt-12 md:mt-0">
+						{children}
+					</div>
+				</div>
+      </div>
     </div>
   );
 }
